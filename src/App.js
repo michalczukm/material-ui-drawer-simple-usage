af49a42d16905ae9bed98a229d802f73ce77979d
Michal Michalczuk <michalczukm@gmail.com>
2020-07-05 19:11:54 +0200

    example of use material=ui drawer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 
-function App() {
+export default () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDrawer = () => setIsOpen(!isOpen);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button onClick={toggleDrawer}>Open!</Button>
+      <Drawer open={isOpen} onClose={toggleDrawer} anchor="left">
+        <List>
+          <ListItem button onClick={toggleDrawer}>
+            <ListItemText primary="Inbox" />
+          </ListItem>
+          
+          <ListItem button onClick={toggleDrawer}>
+            <ListItemText primary="Another one" />
+          </ListItem>
+        </List>
+      </Drawer>
     </div>
   );
 }
-
-export default App;
